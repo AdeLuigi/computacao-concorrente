8a789fd11068fb6d5bb506b0e4bff4f18be07dbf
adeluigi <ademario63@gmail.com>
2021-01-13 18:01:50 -0300

    numerical conc

--- Trabalho1/numerical_conc.c
+++ Trabalho1/numerical_conc.c
@@ -1,176 +1,206 @@
+/********************************************************************* 
+ Example 	: pthread-numerical-integration.c
+ Objective 	: Calculate the function x² Value using Trapezoidal Rule Integration 
+ Input      : Number Of Intervals, Initial limit and Final limit;
+ Output     :Result using Trapezoidal rule
+	         Time Taken for Trapezoidal Computation(in Seconds).
+ Created      : January-2021
+    
+***********************************************************************/
+
 #include<stdio.h>
 #include<stdlib.h>
-#include <pthread.h>
-#include "timer.h"
+#include<math.h>
+#include<pthread.h>
 
 
-float **mat1; //primeira matriz
-float **mat2; //segunda matriz
-float **mat_result; // matriz que armazenará o resultado da multiplicação
-int nThreads; // numero de threads
+
+
+ /*-----------------------------------------Global Variables-----------------------------------------------*/
+
+pthread_mutex_t mutexsum;
+double sum_conc = 0.0, h;
+double sum_seq = 0.0;
+float *arr;
+int nthreads;
+
+
+/* Define the function to be integrated here: */
+#define function(x) (x*x)
+
+
+
+/*---------------------------------------------Data Struct----------------------------------------------*/
+
 
 struct parametros {
-    int id;
-    int dimensao;
+    long long int id;
+    long long int dimensao;
+    double x;
+    double y;
 };
 
-void *multiplicador(void * myArg) {
-    struct parametros *tArgs = (struct parametros *) myArg;
-    printf("Thread %d\n", tArgs->id);
-    double resultado;
 
-    for(int i= tArgs->id; i < tArgs->dimensao; i+=nThreads){
-        for(int j = tArgs->id; j< tArgs->dimensao; j +=nThreads){
-            resultado = 0;
-            for(int k = 0; k < tArgs->dimensao; k++)
-                resultado = resultado + mat1[i][k]*mat2[k][j];
+/*---------------------------------------------Thread Function----------------------------------------------*/
 
-        mat_result[i][j] = resultado;  
-        }
+void *calcularTrapezoidal(void * arg) {
+    struct parametros *tArgs = (struct parametros *) arg;
+    long long int tamBLoco = tArgs ->dimensao/nthreads; // tamanho do bloco de cada thread
+    long long int ini = tArgs->id*tamBLoco; //Elemento inicial do bloco da thread
+    long long int final;
+    double tmp = 0.0;
+    if(tArgs->id == nthreads-1) {
+        final = tArgs->dimensao;
     }
-    pthread_exit(NULL);
-}
-
-
-//fluxo principal
-
-int main(int argc, char** argv) {
-    for (int i = 0; i < 5; i++) {
+    else{
+        final = ini + tamBLoco;
+    }
     
-    int dimensao; //dimensao das duas matrizes
-    pthread_t *tid; //contabilização de threads do sistema
-    struct parametros *args; //estrutura de dados com id e dimensao
-
-    //calculo de tempo
-
-    double inicio, fim, delta, total = 0;
-
-    GET_TIME(inicio);
-
-//usuário irá digitar o número de linha e colunas da matriz quadrada, além do número de threads
-    if(argc < 3){
-     {printf("Digite: %s <dimensao da matriz> <numero de threads>\n", argv[0]);
-     return 1; 
-    }
-    }
-
-    dimensao = atoi(argv[1]);
-    nThreads = atoi(argv[2]);
-    if(nThreads > dimensao) nThreads = dimensao;
-
-
-    // alocação de memória nas linhas das matrizes
-    mat1 = (float**)malloc(sizeof(float*) * dimensao);
-    if(mat1 == NULL) {printf("ERRO--malloc"); return 2;}
-    
-    mat2 = (float**)malloc(sizeof(float*) * dimensao);
-    if(mat2 == NULL) {printf("ERRO--malloc"); return 2;}
-
-    mat_result = (float**)malloc(sizeof(float*) * dimensao);
-    if(mat_result == NULL) {printf("ERRO--malloc"); return 2;}
-
-    //alocação de memória nas colunas das matrizes
-    for(int i = 0; i < dimensao; i++) {
-        mat1[i] = (float*)malloc(sizeof(float) * dimensao);
-        mat2[i] = (float*)malloc(sizeof(float) * dimensao);
-        mat_result[i] = (float*)malloc(sizeof(float) * dimensao);
-
-        //Checando alguma nullpointerexception após a alocação de memória
-        if(mat1[i] == NULL) {printf("ERRO--malloc"); return 2;}
-        if(mat2[i] == NULL) {printf("ERRO--malloc"); return 2;}
-        if(mat_result[i] == NULL) {printf("ERRO--malloc"); return 2;}
-
-    }
- 
+     
+    for(long long int i = ini; i < final; i++) {
 
         
+        tmp += function(arr[i]);
+    }
 
-    //inicialização das estruturas de dados de entrada e saida
-    for(int i = 0; i < dimensao; i++)
-        for(int j = 0; j < dimensao; j++) {
-            mat1[i][j] = 1;
-            mat2[i][j] = 1;
-            mat_result[i][j] = 0;
-        }
+    pthread_mutex_lock(&mutexsum);
+        
+        sum_conc += tmp;
+        pthread_mutex_unlock(&mutexsum);
 
-    GET_TIME(fim);
-    delta = fim - inicio;
-    total +=delta;
-    printf("Tempo inicialização: %lf\n", delta);
+    pthread_exit(NULL);
 
+  }
+
+ 
+/*Program begins*/
+int main(int argc, char** argv){
+  struct parametros *args;
+  pthread_t *tid;
+  long long int dim;
+  clock_t start1, end1, start2, end2;
+  double  time2, time1;;
+  double integral_conc, integral_seq ,a1,b1, x;
+
+    if(argc < 2) {
+        fprintf(stderr,"ERRO-Invalid number argument <Name> <nthreads>");
+        return 3;
+    }
+
+    nthreads = atoi(argv[1]);
     
-    //alocando estruturas2
-
-    GET_TIME(inicio);
-
-    tid =  (pthread_t*)malloc(sizeof(pthread_t) * nThreads);
-    if(tid == NULL){printf("ERRO--malloc"); return 2;}
-    args =  (struct parametros *)malloc(sizeof(struct parametros) * nThreads);
-    if(args == NULL){printf("ERRO--malloc"); return 2;}
 
 
-    //criacao das threads
 
-    for(int i = 0; i < nThreads; i++) {
-        (args + i)->id = i;
-        (args+i)->dimensao = dimensao;
-        if(pthread_create((tid+i), NULL, multiplicador, (void*) (args+i))) {
-            puts("Erro -- pthread create");
-            return 3;
-        }
+  /*Ask the user for necessary input */
+  printf("\nEnter the no. of sub-intervals: ");
+  scanf("%lld",&dim);
+  printf("\nEnter the initial limit: ");
+  scanf("%lf",&a1);
+  printf("\nEnter the final limit: ");
+  scanf("%lf",&b1);
+
+
+  h = fabs(b1-a1)/dim;
+
+/*--------------------------------------------Data Allocation------------------------------------------------*/
+args =(struct parametros *) malloc(sizeof(struct parametros) *nthreads);
+    if(args == NULL) {
+        fprintf(stderr, "ERRO-malloc\n");
+    return 2;
     }
 
-    //espera pelo termino das threads
+tid = (pthread_t *) malloc(sizeof(pthread_t) * nthreads);
+    if(tid == NULL) {
+        fprintf(stderr, "ERRO--malloc\n");
+    return 2;
+}
 
-    for(int i = 0; i< nThreads; i++) {
-        pthread_join(tid[i], NULL);
-    }
+arr = (float *) malloc(sizeof(float)*dim);
+    if(arr == NULL) {
+        fprintf(stderr, "ERRO-malloc\n");
+}
 
-    GET_TIME(fim);
-    delta = fim - inicio;
-    total +=delta;
-
-    printf("Tempo Multiplicacao: %lf\n", delta);
-
-
-
-    //exibição dos resultados
-    GET_TIME(inicio);
-
-
-    //  puts("Matriz de saída");
-    // for(int i = 0; i < dimensao; i++) {
-    //      for (int j = 0; j < dimensao; j++) {
-    //         printf("%.1f ", mat_result[i][j]);
-    //         printf(" ");
-    //    }
-    //      printf("\n");
-    //   }
-
-
-    //liberar memória
-
-    for(int i =0; i < dimensao; i++) {
-        free(mat1[i]);
-        free(mat2[i]);
-        free(mat_result[i]);
-    }
-        free(mat1);
-        free(mat2);
-        free(mat_result);
-
-
-    GET_TIME(fim);
-    delta = fim - inicio;
-    total +=delta;
-
-    printf("Tempo finalizacao: %lf\n", delta);
-    printf("Tempo total %.3lf\n", total);
-
-    }
-    return 0;
-
-
+for(int i = 0; i < dim; i++) {
+        arr[i] = a1 + i*h;
     
+    }
+
+/*--------------------------------------------Sequential Solution--------------------------------------------*/
+start1 = clock();
+
+   for(int i=0;i<dim;i++){
+     x = a1+i*h;
+     sum_seq=sum_seq+function(x);
+   }
+   integral_seq=(h/2)*(function(a1)+function(b1)+2*sum_seq);
+
+
+ end1 = clock();
+time1 = ((double) (end1 - start1)) / CLOCKS_PER_SEC;
+
+
+
+/*--------------------------------------------Concurrent Solution--------------------------------------------*/
+start2 = clock();
+
+
+      
+      pthread_mutex_init(&mutexsum, NULL);
+
+
+/*---------------------------------------Theads Creation-------------------------------------*/
+
+for(long long int i = 0; i < nthreads; i++) {
+    (args + i)->dimensao = dim;
+    (args + i)->id = i;
+
+    (args + i)->x = a1;
+    (args + i)->y= b1;
+
+    if(pthread_create((tid + i), NULL, calcularTrapezoidal, (void*)(args+i))) {
+        fprintf(stderr, "ERRO--pthread_create\n");
+        return 3;
+    }
+}
+
+/*---------------------------------------Waiting for the threads--------------------------------*/
+for(long long int i = 0; i < nthreads; i++) {
+    if(pthread_join(*(tid + i), NULL)) {
+        fprintf(stderr, "ERRO--pthread_join\n");
+        return 3;
+    }
+
+}
+
+/*---------------------------------------Calculate Numerical Integral--------------------------------*/
+
+integral_conc=(h/2)*(function(a1)+function(b1)+2*sum_conc);
+
+
+pthread_mutex_destroy(&mutexsum);
+
+free(tid);
+free(args);
+free(arr);
+
+
+end2 = clock();
+time2 = ((double) (end2 - start2)) / CLOCKS_PER_SEC;
+
+
+/*-------------------------------------------------Print the answer--------------------------------------- */
+printf("\nThe Sequencial Integral is: %lf\n",integral_seq);
+printf("\nThe Concorrency Integral is: %lf\n",integral_conc);
+
+
+printf("\nTime_Seq = %lfs\n", time1);
+printf("\nTime_Conc = %lfs\n", time2);
+
+/*-------------------------------------------------Print performance Gain--------------------------------------- */
+
+printf("\nThe Performance Gain Are: %lf*100\n",time1/time2);
+
+return 0;
 
 }
